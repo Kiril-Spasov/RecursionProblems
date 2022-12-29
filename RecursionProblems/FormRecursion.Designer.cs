c2bd33dcbb4cf92ab0edb3b7dc5e22af0efbb849
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-29 19:34:52 +0200

    Add project files.

---
+++ RecursionProblems/FormRecursion.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RecursionProblems
+{
+    partial class FormRecursion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnProblem1 = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnProblem2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnProblem1
+            // 
+            this.BtnProblem1.Location = new System.Drawing.Point(66, 61);
+            this.BtnProblem1.Name = "BtnProblem1";
+            this.BtnProblem1.Size = new System.Drawing.Size(189, 68);
+            this.BtnProblem1.TabIndex = 0;
+            this.BtnProblem1.Text = "Problem 1";
+            this.BtnProblem1.UseVisualStyleBackColor = true;
+            this.BtnProblem1.Click += new System.EventHandler(this.BtnProblem1_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(291, 61);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(443, 342);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // BtnProblem2
+            // 
+            this.BtnProblem2.Location = new System.Drawing.Point(66, 152);
+            this.BtnProblem2.Name = "BtnProblem2";
+            this.BtnProblem2.Size = new System.Drawing.Size(189, 68);
+            this.BtnProblem2.TabIndex = 2;
+            this.BtnProblem2.Text = "Problem 2";
+            this.BtnProblem2.UseVisualStyleBackColor = true;
+            this.BtnProblem2.Click += new System.EventHandler(this.BtnProblem2_Click);
+            // 
+            // FormRecursion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnProblem2);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnProblem1);
+            this.Name = "FormRecursion";
+            this.Text = "Recursion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnProblem1;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnProblem2;
+    }
+}
+
---
+++ RecursionProblems/FormRecursion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RecursionProblems
+{
+    public partial class FormRecursion : Form
+    {
+        public FormRecursion()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProblem1_Click(object sender, EventArgs e)
+        {
+            TxtResult.Clear();
+
+            //Find out the first 10 numbers of the Fibonacci sequence
+            for (long counter = 1; counter <= 10; counter++)
+            {
+                TxtResult.Text += counter + "\t" + Fibonacci(counter) + Environment.NewLine;
+            }
+        }
+
+        private long Fibonacci(long n)
+        {
+            //First and second numbers of the Fibonacci sequence are 1,1,
+            //so for 1st and 2nd number of the sequence we should return 1.
+            //This is the base case.
+            if (n <= 2)
+            {
+                return 1;
+            }
+            //Recursion step.
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        private void BtnProblem2_Click(object sender, EventArgs e)
+        {
+            TxtResult.Clear();
+
+            long number = 0;
+
+            try
+            {
+                number = Convert.ToInt64(Microsoft.VisualBasic.Interaction.InputBox("Enter a number: ", "Number"));
+
+                TxtResult.Text += "The square of " + number + " is " + Square(number);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("You must enter a number.");
+            }
+        }
+
+
+        private long Square(long n)
+        {
+            //Base case. Square(1) = 1.
+            if (n == 1)
+            {
+                return 1;
+            }
+            //Recursion step. Using the formula: Square(n) = square(n-1) + 2n-1.
+            else
+            {
+                return Square(n - 1) + 2 * n - 1;
+            }
+        }
+    }
+}
